a73a535ed8a177bd1d1d9e31139a3a4950c9f498
Alexanderius <alexander.krylkov@gmail.com>
2016-08-24 10:15:50 +0600

    Handler and responses added

---
+++ Simplify.Web.MessageBox/MessageBoxStatus.cs
@@ -0,0 +1,23 @@
+﻿namespace Simplify.Web.MessageBox
+{
+	/// <summary>
+	/// MessageBox status
+	/// </summary>
+	public enum MessageBoxStatus
+	{
+		/// <summary>
+		/// The information status
+		/// </summary>
+		Information = 0,
+
+		/// <summary>
+		/// The error status
+		/// </summary>
+		Error = 1,
+
+		/// <summary>
+		/// The OK status
+		/// </summary>
+		Ok = 2
+	}
+}
---
+++ Simplify.Web.MessageBox/Responses/MessageBoxInline.cs
@@ -0,0 +1,48 @@
+﻿namespace Simplify.Web.MessageBox.Responses
+{
+	/// <summary>
+	/// Provides inline message box response (generate inline message box and sends it to the user only, without site generation)
+	/// </summary>
+	public class MessageBoxInline : ControllerResponse
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Web.MessageBox.Responses.MessageBox"/> class.
+		/// </summary>
+		/// <param name="text">The message box text.</param>
+		/// <param name="status">The message box status.</param>
+		public MessageBoxInline(string text, MessageBoxStatus status = MessageBoxStatus.Error)
+		{
+			Text = text;
+			Status = status;
+		}
+
+		/// <summary>
+		/// Gets the text.
+		/// </summary>
+		/// <value>
+		/// The text.
+		/// </value>
+		public string Text { get; }
+
+		/// <summary>
+		/// Gets the status.
+		/// </summary>
+		/// <value>
+		/// The status.
+		/// </value>
+		public MessageBoxStatus Status { get; }
+
+		/// <summary>
+		/// Processes this response
+		/// </summary>
+		/// <returns></returns>
+		public override ControllerResponseResult Process()
+		{
+			var handler = new MessageBoxHandler(TemplateFactory, StringTableManager, DataCollector);
+
+			ResponseWriter.Write(handler.GetInline(Text, Status), Context.Response);
+
+			return ControllerResponseResult.RawOutput;
+		}
+	}
+}
